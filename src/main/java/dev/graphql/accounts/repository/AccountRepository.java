fefad709c3de3578295cbaf05e7d6fc9a1197a15
Subhajit Barat <subhajit.barat@gmail.com>
2022-09-04 17:11:31 +0400

    Updated Version

--- src/main/java/dev/graphql/accounts/repository/AccountRepository.java
+++ src/main/java/dev/graphql/accounts/repository/AccountRepository.java
@@ -32,9 +32,9 @@ public class AccountRepository {
 
     @PostConstruct
     private void init() {
-        accounts.add(new Account(1111, "1111111", "SwiftCode", "ArabBank", new Date() , new Date(), 2.0, "Active", "JOD", "P001", new Balances(1000.0,1000.0,0.0,1000.0, new Date()), transactionRepository.findByAccountNumber("1111")));
-        accounts.add(new Account(2222, "2222222", "SwiftCode", "ArabBank", new Date() , new Date(), 2.0, "Active", "JOD", "P001", new Balances(1000.0,1000.0,0.0,1000.0, new Date()), transactionsList));
-        accounts.add(new Account(3333, "3333333", "SwiftCode", "ArabBank", new Date() , new Date(), 2.0, "Active", "JOD", "P001", new Balances(1000.0,1000.0,0.0,1000.0, new Date()), transactionsList));
+        accounts.add(new Account(199432, "1994321111111", "SwiftCode", "ArabBank", new Date() , new Date(), 2.0, "Active", "JOD", "P001", new Balances(132.0,132.0,0.0,132.0, new Date()), transactionRepository.findByAccountNumber("1111")));
+        accounts.add(new Account(199433, "1994322222222", "SwiftCode", "ArabBank", new Date() , new Date(), 2.0, "Active", "JOD", "P001", new Balances(5200.0,5200.0,0.0,5200.0, new Date()), transactionsList));
+        accounts.add(new Account(199434, "1994323333333", "SwiftCode", "ArabBank", new Date() , new Date(), 2.0, "Active", "JOD", "P001", new Balances(1923.0,1923.0,0.0,1923.0, new Date()), transactionsList));
 
         System.out.println("accounts: "+accounts.toString());
     }
